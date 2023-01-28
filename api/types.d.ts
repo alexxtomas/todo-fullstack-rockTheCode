fffbf06cb2900ebef4905e0cb06d498006c76d5b
alexxtomas <atomasllimos@gmail.com>
2023-01-28 18:27:15 +0100

    Create Evirnonment iterface

--- api/types.d.ts
+++ api/types.d.ts
@@ -1,5 +1,15 @@
 declare namespace Express {
-	export interface Request {
-		userId: string
-	}
+  export interface Request {
+    userId: string
+  }
+}
+
+interface IEnvironment {
+  URI: string | undefined
+  message: string
+}
+export interface IEnvironments {
+  development: IEnvironment
+  testing: IEnvironment
+  production: IEnvironment
 }
