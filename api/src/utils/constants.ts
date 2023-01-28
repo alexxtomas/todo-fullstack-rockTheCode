ca6c3cdb3f92e2dd3305f7a29b7df7aa3f134d5b
alexxtomas <atomasllimos@gmail.com>
2023-01-28 22:23:58 +0100

    Add name field to constant

--- api/src/utils/constants.ts
+++ api/src/utils/constants.ts
@@ -1,19 +1,34 @@
 import { IEnvironments } from '../../types.js'
+import { getEnvironment } from './logic.js'
 
 const { MONGODB_DEVELOPMENT_URI, MONGODB_TESTING_URI, MONGODB_PRODUCTION_URI } =
   process.env
 
 export const ENVIRONMENTS: IEnvironments = {
   development: {
+    name: 'Development',
     URI: MONGODB_DEVELOPMENT_URI,
     message: 'DEVELOPMENT MODE 💻'
   },
   testing: {
+    name: 'Testing',
     URI: MONGODB_TESTING_URI,
     message: 'TESTING MODE 🧪'
   },
   production: {
+    name: 'Production',
     URI: MONGODB_PRODUCTION_URI,
     message: 'WARNING PRODUCTION MODE❗'
   }
 }
+
+export const ALLOWED_FORMATS = [
+  'image/jpg',
+  'image/png',
+  'image/jpeg',
+  'image/webp'
+]
+
+export const CLOUDINARY_FOLDER = `todo-fullstack-rockTheCode/${
+  getEnvironment().name
+}`
