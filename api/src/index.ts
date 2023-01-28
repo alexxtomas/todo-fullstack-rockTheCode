c1efa3ccccbab83e16209ec216e04c975ac0d9e5
alexxtomas <atomasllimos@gmail.com>
2023-01-28 01:31:17 +0100

    Test all work fine

---
+++ api/src/index.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from 'express'
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+	return res.send('Hello')
+})
+
+app.listen(3000, () => console.log('Server runnin on port 3000'))
