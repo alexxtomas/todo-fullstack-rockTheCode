4d27f7c74ed67d2234737f4480a3a18afb18cda3
alexxtomas <atomasllimos@gmail.com>
2023-01-28 22:26:13 +0100

    Set up cloudinary in server and add auth router

--- api/src/servers/app.ts
+++ api/src/servers/app.ts
@@ -1,10 +1,14 @@
 import cors from 'cors'
-import express from 'express'
+import express, { Application } from 'express'
 import morgan from 'morgan'
 import '../db/connection.js'
 import authRouter from '../routes/auth.routes.js'
 import userRouter from '../routes/user.routes.js'
-const app = express()
+import cloudinary from '../services/cloudinary.js'
+
+const app: Application = express()
+
+cloudinary.setUp()
 
 app.set('port', process.env.PORT ?? 3001)
 
@@ -12,8 +16,7 @@ app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
 
-// app.use('/api/todo')
-app.use('/api/users', userRouter)
 app.use('/api/auth', authRouter)
+app.use('/api/users', userRouter)
 
 export default app
