84e582a561abf0040810b28119b2d64c171551bd
alexxtomas <atomasllimos@gmail.com>
2023-01-28 22:25:34 +0100

    Add getEnvironment and getCloudinaryKeys functions

--- api/src/utils/logic.ts
+++ api/src/utils/logic.ts
@@ -1,14 +1,34 @@
 import { IEnvironments } from '../../types.js'
 import { ENVIRONMENTS } from './constants.js'
-export function getURI() {
+
+export function getEnvironment() {
   const { NODE_ENV } = process.env
   const environment = ENVIRONMENTS[NODE_ENV as keyof IEnvironments]
+
+  return environment
+}
+
+export function getURI() {
+  const environment = getEnvironment()
   if (!environment) {
     throw new Error('Invalid environment ❌')
   }
   if (!environment.URI) {
-    throw new Error(`No Environment variable defined for ${NODE_ENV} ❌`)
+    throw new Error(
+      `No URI environment variable defined for ${environment.name} ❌`
+    )
   }
   console.log(environment.message)
   return environment.URI
+}
+
+export function getCloudinrayKeys() {
+  const { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } =
+    process.env
+  if (!CLOUDINARY_CLOUD_NAME || !CLOUDINARY_API_KEY || !CLOUDINARY_API_SECRET) {
+    throw new Error(
+      'No cloudinary keys available, please set your keys in file .env'
+    )
+  }
+  return { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET }
 }
