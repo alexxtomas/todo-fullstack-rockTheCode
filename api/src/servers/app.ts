25f471f7be1de95f0ee456217f40c728f3a5d726
alexxtomas <atomasllimos@gmail.com>
2023-01-28 17:50:27 +0100

    Add user route and connect to database

--- api/src/servers/app.ts
+++ api/src/servers/app.ts
@@ -1,20 +1,18 @@
 import cors from 'cors'
 import express from 'express'
 import morgan from 'morgan'
-
+import '../db/connection.js'
+import userRouter from '../routes/user.routes.js'
 const app = express()
 
 app.set('port', process.env.PORT ?? 3001)
 
 app.use(cors())
 app.use(express.json())
-// app.use(morgan('dev'))
+app.use(morgan('dev'))
 
-app.get('/', (req, res) => {
-  return res.send('Raonaaaaa')
-})
 // app.use('/api/todo')
-// app.use('/api/users')
+app.use('/api/users', userRouter)
 // app.use('/api/auth')
 
 export default app
