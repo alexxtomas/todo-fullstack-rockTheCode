6a2e20c8dfbb25e7064b2666e8b5c597e897f862
alexxtomas <atomasllimos@gmail.com>
2023-01-29 00:03:53 +0100

    Add getAllTodo and deleteTodo controllers

--- api/src/controllers/todo.controller.ts
+++ api/src/controllers/todo.controller.ts
@@ -1,6 +1,13 @@
 import { Request, Response } from 'express'
 import { Todo } from '../models/todo.model.js'
+import cloudinary from '../services/cloudinary.js'
 import { ALLOWED_FORMATS } from '../utils/constants.js'
+
+export async function getAllTodo(req: Request, res: Response) {
+  const todos = await Todo.find()
+  res.status(!todos.length ? 204 : 200).json(todos)
+}
+
 export async function createTodo(req: Request, res: Response) {
   const newTodo = new Todo(req.body)
   const { file } = req
@@ -17,4 +24,14 @@ export async function createTodo(req: Request, res: Response) {
   }
   const savedTodo = await newTodo.save()
   res.status(201).json(savedTodo)
+}
+
+export async function deleteTodo(req: Request, res: Response) {
+  const { id } = req.params
+  const deletedTodo = await Todo.findByIdAndDelete(id)
+  if (deletedTodo?.image && deletedTodo?.image !== '') {
+    console.log(deletedTodo.image)
+    await cloudinary.deleteFile(deletedTodo.image)
+  }
+  res.status(200).json(deletedTodo)
 }
