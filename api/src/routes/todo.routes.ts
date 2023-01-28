5ba36149fd750509cc8f8945b9fba8bc492af4dc
alexxtomas <atomasllimos@gmail.com>
2023-01-29 00:04:17 +0100

    Use deleteTodo and getAlltodo in todo routes

--- api/src/routes/todo.routes.ts
+++ api/src/routes/todo.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express'
-import { createTodo } from '../controllers/todo.controller.js'
+import {
+  createTodo,
+  deleteTodo,
+  getAllTodo
+} from '../controllers/todo.controller.js'
 import { upload } from '../middlewares/upload.middleware.js'
 
 const router = Router()
 
-router.get('/')
+router.get('/', getAllTodo)
 router.post('/', upload.single('image'), createTodo)
-
+router.delete('/:id', deleteTodo)
 export default router
