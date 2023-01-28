12a2ad179cd5279669b93979004e573fb0f5580d
alexxtomas <atomasllimos@gmail.com>
2023-01-28 16:25:44 +0100

    Create needed models

---
+++ api/src/models/todo-model.ts
@@ -0,0 +1,28 @@
+import { model, Schema } from 'mongoose'
+import { IUser } from './user-model.js'
+
+export interface ITodo {
+  title: string
+  description: string
+  completed: boolean
+  image: string
+  user: IUser
+}
+
+const todoSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true, default: '' },
+  completed: { type: Boolean, required: true, default: false },
+  image: { type: String, required: true, default: '' },
+  user: { type: Schema.Types.ObjectId, ref: 'User' }
+})
+
+todoSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+export default model<ITodo>('Todo', todoSchema)
---
+++ api/src/models/user-model.ts
@@ -0,0 +1,26 @@
+import { model, Schema } from 'mongoose'
+import { ITodo } from './todo-model.js'
+
+export interface IUser {
+  username: string
+  email: string
+  password: string
+  todoList: ITodo[]
+}
+
+const userSchema = new Schema({
+  username: { type: String, unique: true, required: true, min: 3 },
+  email: { type: String, unique: true, required: true, min: 9 },
+  password: { type: String, required: true, min: 6 },
+  todoList: [{ type: Schema.Types.ObjectId, ref: 'Todo' }]
+})
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+export default model<IUser>('User', userSchema)
