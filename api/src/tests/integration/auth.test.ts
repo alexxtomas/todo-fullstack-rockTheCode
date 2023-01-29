71f21c90343dd5c42adb90bff81c56b2c41ad61f
alexxtomas <atomasllimos@gmail.com>
2023-01-29 18:02:00 +0100

    Create some test for sign up enpoint

---
+++ api/src/tests/integration/auth.test.ts
@@ -0,0 +1,48 @@
+import bcrypt from 'bcrypt'
+import { IUser, User } from '../../models/user.model.js'
+import { appServer, closeConnection, getUsers } from './helpers/shared.js'
+
+describe.skip('AUTH', () => {
+  beforeEach(async () => {
+    await User.deleteMany()
+
+    const passwordHash = await bcrypt.hash('testPassword', 10)
+    const initialUser = new User({
+      username: 'testUser',
+      password: passwordHash
+    })
+
+    await initialUser.save()
+  })
+  // TODO
+  describe.skip('POST /api/auth/login')
+
+  describe('POST /api/auth/sign-up', () => {
+    test('works as expected when user sign up', async () => {
+      const usersAtStart = await getUsers()
+
+      const newUser: IUser = {
+        username: 'New user',
+        password: 'newUserPassword',
+        todoList: []
+      }
+
+      await appServer
+        .post('/api/auth/sign-up')
+        .send(newUser)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      const usersAtEnd = await getUsers()
+
+      const usernames = usersAtEnd.map((user) => user.username)
+
+      expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+      expect(usernames).toContain(newUser.username)
+    })
+  })
+
+  afterAll(async () => {
+    await closeConnection()
+  })
+})
