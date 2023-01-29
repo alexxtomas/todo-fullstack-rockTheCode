e939da19424d0060a0d2369b11969e269bc6b2ac
alexxtomas <atomasllimos@gmail.com>
2023-01-29 18:55:21 +0100

    Create auth validation file

---
+++ api/src/validations/auth.validations.ts
@@ -0,0 +1,12 @@
+import { body } from "express-validator"
+import { User } from "../models/user.model.js"
+
+export const signUpValidator = [
+  body('username')
+    .isString()
+    .isLength({min: 3})
+    .custom(async (value) => {
+      const user = await User.findOne({username: value})
+      if(user)
+    })
+]
