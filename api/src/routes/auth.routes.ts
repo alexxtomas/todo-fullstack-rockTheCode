294a29ec2e5b9a00518ef0b19688ecca8a6d101d
alexxtomas <atomasllimos@gmail.com>
2023-01-29 20:39:05 +0100

    Add validators to auth routes

--- api/src/routes/auth.routes.ts
+++ api/src/routes/auth.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express'
 import { login, signUp } from '../controllers/auth.controller.js'
-
+import {
+  loginValidator,
+  signUpValidator
+} from '../validations/auth.validations.js'
 const router = Router()
 
-router.post('/sign-up', signUp)
-router.post('/login', login)
+router.post('/sign-up', signUpValidator, signUp)
+router.post('/login', loginValidator, login)
 
 export default router
