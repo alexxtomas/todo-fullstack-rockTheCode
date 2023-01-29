efe29475e4846cf6aca9b627c0b70b7e89e7f883
alexxtomas <atomasllimos@gmail.com>
2023-01-29 13:01:38 +0100

    Add users functions to shared file

--- api/src/tests/integration/helpers/shared.ts
+++ api/src/tests/integration/helpers/shared.ts
@@ -1,4 +1,16 @@
+import mongoose from 'mongoose'
 import supertest from 'supertest'
+import { server } from '../../../index.js'
+import { User } from '../../../models/user.model.js'
 import app from '../../../servers/app.js'
-
 export const appServer = supertest(app)
+
+export async function getUsers() {
+  const users = await User.find()
+  return users
+}
+
+export function closeConnection() {
+  mongoose.connection.close()
+  server.close()
+}
