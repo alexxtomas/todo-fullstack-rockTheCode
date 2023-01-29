d53efad5fd78be62d72964af13a34f2492c345c5
alexxtomas <atomasllimos@gmail.com>
2023-01-29 20:38:54 +0100

    Error handling  with validator and errorHandler middleware

--- api/src/controllers/auth.controller.ts
+++ api/src/controllers/auth.controller.ts
@@ -1,30 +1,47 @@
 import bcrypt from 'bcrypt'
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
+import { validationResult } from 'express-validator'
 import jwt from 'jsonwebtoken'
 import { IUser, User } from '../models/user.model.js'
-export async function signUp(req: Request, res: Response) {
-  const { password, username } = req.body
-  const hashedPassword = await bcrypt.hash(password, 10)
-  const user = new User({
-    password: hashedPassword,
-    username
-  })
+export async function signUp(req: Request, res: Response, next: NextFunction) {
+  try {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+    const { password, username } = req.body
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const user = new User({
+      password: hashedPassword,
+      username
+    })
 
-  const savedUser = await user.save()
-  res.status(201).json({ user: savedUser })
+    const savedUser = await user.save()
+    res.status(201).json({ user: savedUser })
+  } catch (error: any) {
+    next(error)
+  }
 }
 
-export async function login(req: Request, res: Response) {
-  const { username } = req.body
-  const { JWT_SECRET } = process.env
-  const user = await User.findOne({ username })
+export async function login(req: Request, res: Response, next: NextFunction) {
+  try {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+    const { username } = req.body
+    const { JWT_SECRET } = process.env
+    const user = await User.findOne({ username })
 
-  const token = jwt.sign(
-    {
-      exp: Math.floor(Date.now() / 1000) * (60 * 60 * 24 * 7),
-      id: user?.id
-    },
-    JWT_SECRET!
-  )
-  res.status(200).json({ token })
+    const token = jwt.sign(
+      {
+        exp: Math.floor(Date.now() / 1000) * (60 * 60 * 24 * 7),
+        id: user?.id
+      },
+      JWT_SECRET!
+    )
+    res.status(200).json({ token })
+  } catch (error) {
+    next(error)
+  }
 }
