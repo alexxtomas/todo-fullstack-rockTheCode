8e900b64c4d7b853970c09da4e0e43776c6bceb1
alexxtomas <atomasllimos@gmail.com>
2023-01-29 20:37:56 +0100

    Create auth validator

--- api/src/validations/auth.validations.ts
+++ api/src/validations/auth.validations.ts
@@ -1,12 +1,13 @@
-import { body } from "express-validator"
-import { User } from "../models/user.model.js"
+import { body } from 'express-validator'
+import { User } from '../models/user.model.js'
 
 export const signUpValidator = [
   body('username')
     .isString()
-    .isLength({min: 3})
+    .isLength({ min: 3 })
     .custom(async (value) => {
-      const user = await User.findOne({username: value})
-      if(user)
+      const user = await User.findOne({ username: value })
+      if (user) throw new Error('Username is not available')
+      return true
     })
 ]
