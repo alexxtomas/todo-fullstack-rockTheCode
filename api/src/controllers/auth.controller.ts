45eef521c11157f70451b59d6a930f8822e7ecd9
alexxtomas <atomasllimos@gmail.com>
2023-01-29 18:53:11 +0100

    Create login controller

--- api/src/controllers/auth.controller.ts
+++ api/src/controllers/auth.controller.ts
@@ -1,7 +1,7 @@
 import bcrypt from 'bcrypt'
 import { Request, Response } from 'express'
-import { User } from '../models/user.model.js'
-
+import jwt from 'jsonwebtoken'
+import { IUser, User } from '../models/user.model.js'
 export async function signUp(req: Request, res: Response) {
   const { password, username } = req.body
   const hashedPassword = await bcrypt.hash(password, 10)
@@ -12,4 +12,19 @@ export async function signUp(req: Request, res: Response) {
 
   const savedUser = await user.save()
   res.status(201).json({ user: savedUser })
+}
+
+export async function login(req: Request, res: Response) {
+  const { username } = req.body
+  const { JWT_SECRET } = process.env
+  const user = await User.findOne({ username })
+
+  const token = jwt.sign(
+    {
+      exp: Math.floor(Date.now() / 1000) * (60 * 60 * 24 * 7),
+      id: user?.id
+    },
+    JWT_SECRET!
+  )
+  res.status(200).json({ token })
 }
