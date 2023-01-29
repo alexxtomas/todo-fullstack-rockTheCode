c5bbd9989b43954f3b8428d8fab3f763dc49f6cb
alexxtomas <atomasllimos@gmail.com>
2023-01-29 11:45:22 +0100

    Export app server

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,5 @@
-import 'dotenv/config.js'
 import app from './servers/app.js'
 
-app.listen(app.get('port'), () =>
+export const server = app.listen(app.get('port'), () =>
   console.log(`Server running on port ${app.get('port')} 🚀`)
 )
