ca7363fdb4e319fc67d79cb71c14fa2052b2440c
alexxtomas <atomasllimos@gmail.com>
2023-01-29 18:01:25 +0100

    Add needed content to users helper

--- api/src/tests/integration/helpers/users.ts
+++ api/src/tests/integration/helpers/users.ts
@@ -1,2 +1,36 @@
+import bcrypt from 'bcrypt'
+import { IUser, User } from '../../../models/user.model.js'
+export const initialUsers: IUser[] = [
+  {
+    username: 'pepe',
+    password: 'pepePassword',
+    todoList: []
+  },
+  {
+    username: 'ramon',
+    password: 'ramonPassword',
+    todoList: []
+  },
+  {
+    username: 'ramiro',
+    password: 'ramiroPassword',
+    todoList: []
+  },
+  {
+    username: 'sandra',
+    password: 'sandraPassword',
+    todoList: []
+  }
+]
 
+export function getInitialUsers() {
+  return initialUsers.map(({ password, ...restOfUser }) => ({
+    ...restOfUser,
+    password: bcrypt.hashSync(password, 10)
+  }))
+}
 
+export async function getOneUser() {
+  const user = await User.findOne()
+  return user
+}
