a7a0dbe918fb81ff12bcc866bcb76cf7e953b5ae
alexxtomas <atomasllimos@gmail.com>
2023-01-29 18:00:33 +0100

    Use only one thread

--- api/vitest.config.ts
+++ api/vitest.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    globals: true
+    globals: true,
+    threads: true,
+    maxThreads: 1,
+    minThreads: 1
   }
 })
