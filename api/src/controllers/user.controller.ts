6d84ee278889e137acc62271589731ac24f35620
alexxtomas <atomasllimos@gmail.com>
2023-01-29 18:00:47 +0100

    Add function to get user by id

--- api/src/controllers/user.controller.ts
+++ api/src/controllers/user.controller.ts
@@ -5,3 +5,11 @@ export async function getAllUsers(_req: Request, res: Response) {
   const users = await User.find()
   res.status(!users.length ? 204 : 200).json(users)
 }
+
+export async function getUserById(req: Request, res: Response) {
+  const { id } = req.params
+  const user = await User.findById(id)
+  if (!user)
+    return res.status(400).json({ error: 'No existent user with this id' })
+  res.status(200).json(user)
+}
