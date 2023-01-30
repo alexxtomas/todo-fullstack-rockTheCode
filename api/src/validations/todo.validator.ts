87e390be769af1c06084b6978b772c561414a467
alexxtomas <atomasllimos@gmail.com>
2023-01-30 10:57:51 +0100

    Create todo validators

---
+++ api/src/validations/todo.validator.ts
@@ -0,0 +1,30 @@
+import { body, param } from 'express-validator'
+import { Todo } from '../models/todo.model.js'
+
+export const idValidator = [
+  param('id')
+    .isString()
+    .notEmpty()
+    .custom(async (value) => {
+      const todo = await Todo.findById(value)
+      if (!todo) throw new Error('no existent todo with this id')
+      return true
+    })
+]
+
+export const createTodoValidator = [
+  body('title')
+    .isString()
+    .withMessage('title should be a string')
+    .notEmpty()
+    .withMessage('title is a required field'),
+  body('description')
+    .optional()
+    .isString()
+    .withMessage('description should be a string'),
+
+  body('completed')
+    .optional()
+    .isBoolean()
+    .withMessage('completed field should be a boolean value')
+]
