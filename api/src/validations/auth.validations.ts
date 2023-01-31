fcdae2219bd182e2ff94cc714952224b80d75fc2
alexxtomas <atomasllimos@gmail.com>
2023-01-31 09:00:13 +0100

    Remove unnecessary login validations

--- api/src/validations/auth.validations.ts
+++ api/src/validations/auth.validations.ts
@@ -21,27 +21,17 @@ export const signUpValidator = [
 ]
 
 export const loginValidator = [
-  body('username')
-    .isString()
-    .withMessage('username should be a string and is required')
-    .isLength({ min: 3 })
-    .withMessage('username must have a minimum length of 3')
-    .custom(async (value, { req }) => {
-      const user = await User.findOne({ username: value })
-      if (!user) throw new Error('invalid credentials')
-      return true
-    }),
-  body('password')
-    .isString()
-    .withMessage('password should be a string and is required')
-    .isLength({ min: 6 })
-    .withMessage('password must have a minimum length of 6')
-    .custom(async (value, { req }) => {
-      const { username } = req.body
-      const user = await User.findOne({ username })
-      const isAValidPassword = await bcrypt.compare(value, user?.password ?? '')
-      if (!isAValidPassword) throw new Error('invalid credentails')
+  body('username').custom(async (value, { req }) => {
+    const user = await User.findOne({ username: value })
+    if (!user) throw new Error('invalid credentials')
+    return true
+  }),
+  body('password').custom(async (value, { req }) => {
+    const { username } = req.body
+    const user = await User.findOne({ username })
+    const isAValidPassword = await bcrypt.compare(value, user?.password ?? '')
+    if (!isAValidPassword) throw new Error('invalid credentails')
 
-      return true
-    })
+    return true
+  })
 ]
