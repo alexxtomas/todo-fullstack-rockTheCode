abacdeaaadec4a89a8d46ba86e67fc9d06ca2ad0
alexxtomas <atomasllimos@gmail.com>
2023-02-01 22:23:04 +0100

    Create router

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import PageLayout from './components/layouts/PageLayout'
+import Error from './pages/Error'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
 import './styles/global.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+
+    errorElement: <Error />
+  },
+  {
+    path: '/auth/login',
+    element: <Login />
+  },
+  {
+    path: '/auth/sign-up',
+    element: <SignUp />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <PageLayout title="Todo List 🚀">
+      <RouterProvider router={router} />
+      {/* <App /> */}
+    </PageLayout>
   </React.StrictMode>
 )
